410e2da57c153560907b4619ade23a9cb5671beb
noaaviran <154530417+noaaviran@users.noreply.github.com>
2024-01-15 09:56:52 +0200

    Add files via upload

--- ArrayOps.java
+++ ArrayOps.java
@@ -69,7 +69,7 @@ public class ArrayOps {
          }
          for (int i = 0; i < countArray1.length; i++) {
             int count = countArray1[i];
-            if (count < 0) {
+            if (count != 0) {
                 return false;
         }
     }
